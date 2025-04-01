2f8c07f281af12e17143217e0f46cc24dbe5c5dd
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-04-01 11:20:00 +0200

    fic tailwind e bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,14 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body>
     <h1 class="text-center">Homepage</h1>
 
-
+    @include('partials.navbar')
 </body>
 
 </html>
