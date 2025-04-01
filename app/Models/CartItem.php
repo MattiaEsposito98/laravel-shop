0024b5f78c9089d48094f4f9b93526de4f566cc8
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-04-01 12:01:42 +0200

    add Models con relazioni

---
+++ app/Models/CartItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
