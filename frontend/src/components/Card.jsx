416944265a77d987abec04fdc77c8ce08130c52e
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-04-09 13:11:54 +0200

    add card con immagine e dettagli

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,8 +1,19 @@
+import style from './Card.module.css'
+
 export default function Card({ product }) {
+  console.log(product)
+  const randomImg = 'https://images.unsplash.com/photo-1602143407151-7111542de6e8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
   return (
     <>
+      <div className={style.card}>
+        <img src={product.image ? `http://127.0.0.1:8000/storage/${product.image}` : randomImg} alt={product.name} className={style.img} />
+        <div className={style.cardDetails}>
+          <h4>{product.name}</h4>
+          <p><strong>Prezzo:</strong> {product.price}€</p>
+        </div>
 
+      </div>
     </>
   )
 }
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,19 +4,22 @@ import Card from "../components/Card"
 
 export default function Homepage() {
   const { products } = useContext(GlobalContext)
+  const [currentPage, setCurrentPage] = useState(1); // Iniziamo dalla prima pagina
+  const [productsPerPage] = useState(6); // Mostriamo 6 prodotti per pagina (puoi cambiare questo numero)
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
   return (
     <div className="container">
       <h1>Homepage</h1>
       <div className="row">
-        <div className="col mb-3">
-          <ul>
-            {products.map(product => (
-              <li key={product.id}><Card product={product} /></li>
-            ))}
-          </ul>
-
-        </div>
+        {products.map(product => (
+          <div className="col-md-4 mb-3" key={product.id}>
+            <Card product={product} />
+          </div>
+        ))}
       </div>
     </div>
   )
